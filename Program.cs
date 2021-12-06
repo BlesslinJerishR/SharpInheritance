6e9ace91b7ea8a575d130ae15427c601a912b5b9
Mastermind <mastermindx33@gmail.com>
2021-12-06 17:25:26 +0530

    build @ inheritance

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CanarysInheritanceX1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I am the Parent");
+            Parent op = new Parent();
+            op.DisplayParent();
+
+            Console.WriteLine("I am the Child");
+            Child oc = new Child();
+            oc.DisplayParent();
+            oc.DisplayChild();
+            Console.ReadLine();
+        }
+        public class Parent
+        {
+            public void DisplayParent()
+            {
+                Console.WriteLine("Hey Mom");
+            }
+        }
+
+        public class Child : Parent
+        {
+            public void DisplayChild()
+            {
+                Console.WriteLine("Hey Child");
+            }
+        }
+    }
+ }
